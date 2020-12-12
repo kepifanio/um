a943789ee2295e5811e2de48acbe774567209a21
Kate Epifanio <71147279+kepifanio@users.noreply.github.com>
2020-12-12 01:18:00 -0500

    Add files via upload

---
+++ um_segment.h
@@ -0,0 +1,35 @@
+/**************************************************************
+ *                          um_segment.h
+ *
+ *    Assignment: um
+ *    Date: 11.25.20
+ *
+ **************************************************************/
+
+#ifndef SEGMENT
+#define SEGMENT
+
+#include <seq.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+typedef struct Seq_T *segment_t;
+
+/* Make a sequence of segments */
+segment_t init_seg(int quantity);
+
+/* Frees the segments */
+void free_seg(segment_t segment);
+
+/* Adds the instruction to a sequence of instructions */
+void store_instruction(uint32_t instruction, segment_t program);
+
+/* Returns the instruction at that segment with that index */
+uint32_t get_segment(uint32_t index, segment_t segment);
+
+/* Stores the value at that segment at that index */
+void put_segment(uint32_t index, segment_t segment, uint32_t value);
+
+
+#endif
